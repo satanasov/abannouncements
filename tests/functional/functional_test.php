edbf76430ccbec46d7b2ac4346527b6f9a3fdc36
Stanislav Atanasov <lucifer@anavaro.com>
2022-07-19 22:36:32 +0300

    Try again to make tests work!

--- tests/functional/functional_test.php
+++ tests/functional/functional_test.php
@@ -11,7 +11,7 @@ namespace anavaro\abannouncements\tests\functional;
 /**
 * @group functional
 */
-class functional_test extends phpbb_functional_test_case
+class functional_test extends \phpbb_functional_test_case
 {
 	static protected function setup_extensions()
 	{
